c527039525c979361dbfbd42d559df87063d2be3
caue <caueabproenca@gmail.com>
2025-04-10 14:36:48 -0300

    Fixed import error

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from './store.module';
+import { StoreModule } from './store.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
+    StoreModule,
     new FastifyAdapter({ logger: true }),
   );
   await app.listen(process.env.PORT ?? 3000);
