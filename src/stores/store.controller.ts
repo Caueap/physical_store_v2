8f26d32c50be557af576c949f057c53f51af011b
caue <caueabproenca@gmail.com>
2025-04-11 20:10:51 -0300

    Implemented getStoreById functionality

--- src/stores/store.controller.ts
+++ src/stores/store.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, ValidationPipe, Post, Body } from '@nestjs/common';
+import { Controller, Get, Query, Param, ValidationPipe, Post, Body } from '@nestjs/common';
 import { StoreService } from './store.service';
 import { CreateStoreDto } from './dtos/create-store.dto';
 import { GetAllStoresDto } from './dtos/get-all-stores.dto';
@@ -11,6 +11,11 @@ export class StoreController {
   async getAllStores(@Query(new ValidationPipe({ transform: true })) query: GetAllStoresDto) {
     const { limit = 10, offset = 0 } = query;
     return this.storeService.getAllStores(limit, offset);
+  }
+
+  @Get('id/:id')
+  async getStoreById(@Param('id') id: string) {
+    return this.storeService.getStoreById(id);
   }
 
   @Post()
